092696ef9d45c20afa5cef1e7703b3175257c81a
Kumkum <kumkumverma101@gmail.com>
2025-11-14 16:24:13 +0530

    Add List for the different roles of user

--- src/main/java/net/engineeringdigest/journalApp/repostiory/UserRepository.java
+++ src/main/java/net/engineeringdigest/journalApp/repostiory/UserRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 @Repository
 public interface UserRepository extends MongoRepository<User,ObjectId> {
     User findByUserName(String userName);
+    void deleteByUserName(String userName);
 }
