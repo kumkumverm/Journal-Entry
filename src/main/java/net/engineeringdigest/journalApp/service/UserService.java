9da7984058471ccbfd6b642da9d29157c95a1ee7
Kumkum <kumkumverma101@gmail.com>
2025-11-16 20:38:04 +0530

    Add method to save User in db without encoding

--- src/main/java/net/engineeringdigest/journalApp/service/UserService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/UserService.java
@@ -23,6 +23,9 @@ public class UserService {
       user.setRoles(Arrays.asList("USER"));
       userRepository.save(user);
    }
+   public void saveUser(User user){
+      userRepository.save(user);
+   }
    public List<User> getAll() {
       return userRepository.findAll();
    }
